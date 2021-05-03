d8d70d11178df82a61ed192adfe08a9a18b0070e
username-69 <mathematicalnerd@outlook.com>
2021-05-03 22:35:34 +0530

    Adding 7th.

---
+++ app/src/main/java/com/example/mini/MainActivity7.java
@@ -0,0 +1,19 @@
+package com.example.mini;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity7 extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main7);
+    }
+
+}
